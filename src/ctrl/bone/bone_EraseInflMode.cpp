7461fcafa12ed646edcc903373df6e37c0309e4b
Gambot <56745325+GbotHQ@users.noreply.github.com>
2026-01-30 13:26:16 +0100

    Port * matrix multiplications operators to Qt6 .map

--- src/ctrl/bone/bone_EraseInflMode.cpp
+++ src/ctrl/bone/bone_EraseInflMode.cpp
@@ -107,8 +107,8 @@ namespace bone {
         if (!parent)
             return result;
 
-        const QVector2D brushPos = (mTargetInvMtx * QVector3D(aBrush.center())).toVector2D();
-        const float brushRange = (mTargetInvMtx * QVector3D(mBrush.radius(), 0.0f, 0.0f) - mTargetInvMtx * QVector3D())
+        const QVector2D brushPos = (mTargetInvMtx.map(QVector3D(aBrush.center()))).toVector2D();
+        const float brushRange = (mTargetInvMtx.map(QVector3D(mBrush.radius(), 0.0f, 0.0f)) - mTargetInvMtx.map(QVector3D()))
                                      .toVector2D()
                                      .length();
 
